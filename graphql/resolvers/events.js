2566d16fd2c9869b826b0f1b6d229c95261764ab
villagef <filwyd123@gmail.com>
2020-03-24 18:39:36 +0100

    user authentication, creator userId changed atomat

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,4 +1,5 @@
 const Event = require("../../models/event");
+const User = require("../../models/user");
 
 const { transformEvent } = require("./merge");
 
@@ -13,19 +14,22 @@ module.exports = {
       throw err;
     }
   },
-  createEvent: async args => {
+  createEvent: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated !");
+    }
     const event = new Event({
       title: args.eventInput.title,
       description: args.eventInput.description,
       price: +args.eventInput.price,
       date: new Date(args.eventInput.date),
-      creator: "5e776653aa74b742e4ce8a1d"
+      creator: req.userId
     });
     let createdEvent;
     try {
       const result = await event.save();
       createdEvent = transformEvent(result);
-      const creator = await User.findById("5e776653aa74b742e4ce8a1d");
+      const creator = await User.findById(req.userId);
 
       if (!creator) {
         throw new Error("User not found.");
