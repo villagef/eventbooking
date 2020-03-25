489d2e90754cb3e50c59c2126af06bfca7800ebb
villagef <filwyd123@gmail.com>
2020-03-25 09:50:36 +0100

    create reactjs and routing

---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class BookingsPage extends Component {
+  render() {
+    return <h1>Bookings Should work</h1>;
+  }
+}
+
+export default BookingsPage;
---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class EventsPage extends Component {
+  render() {
+    return <h1>Events Should work</h1>;
+  }
+}
+
+export default EventsPage;
